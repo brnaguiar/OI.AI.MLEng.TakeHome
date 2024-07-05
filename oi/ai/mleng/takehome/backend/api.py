393bdd62b6a90f342581d6660a52fb9c56883556
Bruno Aguiar <bruno.f.o.aguiar@gmail.com>
2024-07-05 14:53:07 +0100

    Update backend

--- oi/ai/mleng/takehome/backend/api.py
+++ oi/ai/mleng/takehome/backend/api.py
@@ -1,11 +1,14 @@
+import os
+import tempfile
+
+import aiohttp
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, File, Form, HTTPException, UploadFile
+from fastapi.responses import HTMLResponse, JSONResponse
 
 from oi.ai.mleng.takehome.pipeline.model import MarineAnimalClassifier
 from oi.ai.mleng.takehome.pipeline.preprocessing import ImagePreprocessor
 
-# Instantiate the classifier
 preprocessor = ImagePreprocessor()
 classifier = MarineAnimalClassifier(preprocessor)
 
@@ -14,19 +17,126 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Marine Animal Classifier API"}
+    content2 = """
+    <html>
+        <head>
+            <title>Marine Animal Predictor</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    background-color: #f2f9ff;
+                    text-align: center;
+                    padding: 50px;
+                }
+                h1 {
+                    color: #003366;
+                }
+                form {
+                    display: inline-block;
+                    background-color: #fff;
+                    padding: 20px;
+                    border: 1px solid #ccc;
+                    border-radius: 5px;
+                }
+                input[type="text"], input[type="submit"] {
+                    margin: 5px;
+                    padding: 10px;
+                    border-radius: 5px;
+                }
+                input[type="text"] {
+                    border: 1px solid #003366;
+                }
+                input[type="submit"] {
+                    background-color: #003366;
+                    color: #fff;
+                    border: none;
+                    cursor: pointer;
+                }
+                input[type="submit"]:hover {
+                    background-color: #00509e;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Marine Animal Predictor</h1>
+            <form action="/predict/" method="post">
+                <input name="url" type="text" placeholder="Enter image URL">
+                <input type="submit" value="Predict">
+            </form>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=content2)
 
 
 @app.post("/predict/")
-async def predict_image(file: UploadFile = File(...)) -> JSONResponse:
-    # Save file
-    temp_file_path = f"/tmp/{file.filename}"
-    with open(temp_file_path, "wb") as f:
-        f.write(file.file.read())
+async def predict_image2(url: str = Form(...)) -> JSONResponse:
+    filename = url.split("/")[-1]
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                content = await response.read()
+                temp_dir = tempfile.gettempdir()
+                temp_file_path = os.path.join(temp_dir, filename)
+                with open(temp_file_path, "wb") as f:
+                    f.write(content)
+            else:
+                raise HTTPException(
+                    status_code=response.status,
+                    detail="Failed to download file",
+                )
 
     # Perform prediction
     class_idx: int = classifier.predict(temp_file_path)
-    return JSONResponse(content={"class_index": class_idx})
+
+    prediction = class_idx 
+    content = f"""
+    <html>
+        <head>
+            <title>Prediction Result</title>
+            <style>
+                body {{
+                    font-family: Arial, sans-serif;
+                    background-color: #e0f7fa;
+                    text-align: center;
+                    padding: 50px;
+                }}
+                h1 {{
+                    color: #003366;
+                }}
+                .result {{
+                    display: inline-block;
+                    background-color: #fff;
+                    padding: 20px;
+                    border: 1px solid #ccc;
+                    border-radius: 5px;
+                }}
+                img {{
+                    max-width: 100%;
+                    height: auto;
+                    border: 1px solid #003366;
+                    border-radius: 5px;
+                }}
+                .prediction {{
+                    margin-top: 10px;
+                    font-size: 1.2em;
+                    color: #003366;
+                }}
+            </style>
+        </head>
+        <body>
+            <h1>Prediction Result</h1>
+            <div class="result">
+                <img src="{url}" alt="Marine Animal Image">
+                <div class="prediction">Prediction: {prediction}</div>
+            </div>
+            <br>
+            <a href="/">Go Back</a>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=content)
+    #return JSONResponse(content={"class_index": class_idx})
 
 
 if __name__ == "__main__":
--- oi/ai/mleng/takehome/pipeline/model.py
+++ oi/ai/mleng/takehome/pipeline/model.py
@@ -1,7 +1,5 @@
 import torch
 import torchvision.models as models
-import torchvision.transforms as transforms
-from PIL import Image
 
 from oi.ai.mleng.takehome.pipeline.preprocessing import ImagePreprocessor
 
